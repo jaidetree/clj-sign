0b89eefe06bacdae9b6883dad183ee0d46801031
Jay Zawrotny <jayzawrotny@gmail.com>
2018-05-29 19:47:43 -0400

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-sign
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_sign/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-sign.core-test
+  (:require [clojure.test :refer :all]
+            [clj-sign.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
