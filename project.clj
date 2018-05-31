aa9ee39e62d40db21592e90132baffb86c87152e
Jay Zawrotny <jayzawrotny@gmail.com>
2018-05-30 23:32:10 -0400

    Removed unused dependencies from deps.edn

--- project.clj
+++ project.clj
@@ -4,8 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [clj-http "3.9.0"]
-                 [cheshire "5.8.0"]
                  [xsc/pem-reader "0.1.1"]]
   :main ^:skip-aot clj-sign.core
   :target-path "target/%s"
